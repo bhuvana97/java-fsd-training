6adbaa1eebd9c336d2133eb60c529638c610b0d6
BHUVANESWARI N <66697555+bhuvana97@users.noreply.github.com>
2022-03-08 09:49:10 +0530

    Add files via upload

---
+++ Typescript/grade.ts
@@ -0,0 +1,23 @@
+var garde:string='A'
+switch(garde){
+    case 'A':{
+        console.log('Excellent')
+        break;
+    }
+    case 'B':{
+        console.log('good')
+        break;
+    }
+    case 'C':{
+        console.log('fair')
+        break;
+    }
+    case 'D':{
+        console.log('poor')
+        break;
+    }
+    default:{
+console.log("default choice")
+break;
+    }
+}
---
+++ Typescript/number.ts
@@ -0,0 +1,12 @@
+var number:number=12;
+console.log(number)
+
+var num:number=2
+if(num){
+    console.log(num+"is positive")
+}else if (num<0){
+    console.log(num+"is negative")
+
+
+}else
+console.log(num+"num is neither positive or negative")
